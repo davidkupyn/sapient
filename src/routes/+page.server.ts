f93bfd0d726d9f18bc4f08b95fbe1accb4d7fff0
davidkupyn <davidkupyn@gmail.com>
2023-10-01 08:02:14 +0200

    cos tam

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,19 +2,19 @@ import type { University } from '$lib/types';
 
 export async function load({ url }) {
 	const searchQuery = url.searchParams.get('search');
-	console.log(searchQuery);
-	// const mode = url.searchParams.has('mode')
-	// 	? JSON.parse(url.searchParams.get('mode') || '')
-	// 	: undefined;
-	// const city = url.searchParams.has('city')
-	// 	? JSON.parse(url.searchParams.get('city') || '')
-	// 	: undefined;
-	// const type = url.searchParams.has('type')
-	// 	? JSON.parse(url.searchParams.get('type') || '')
-	// 	: undefined;
-	// const degree = url.searchParams.has('degree')
-	// 	? JSON.parse(url.searchParams.get('degree') || '')
-	// 	: undefined;
+
+	const mode = url.searchParams.has('mode')
+		? JSON.parse(url.searchParams.get('mode') || '')
+		: undefined;
+	const city = url.searchParams.has('city')
+		? JSON.parse(url.searchParams.get('city') || '')
+		: undefined;
+	const type = url.searchParams.has('type')
+		? JSON.parse(url.searchParams.get('type') || '')
+		: undefined;
+	const degree = url.searchParams.has('degree')
+		? JSON.parse(url.searchParams.get('degree') || '')
+		: undefined;
 
 	if (searchQuery) {
 		const searchJSON = await fetch(`https://sapient-api.kupyn.dev/search?q=${searchQuery}`);
