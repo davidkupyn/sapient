6a8b98e5bb71a69f6c8aad0a5a6c26e010de9483
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2023-10-01 06:28:20 +0200

    University type

--- src/lib/types/university.ts
+++ src/lib/types/university.ts
@@ -1,3 +1,17 @@
 export type University = {
+	id: number;
+	school_id: number;
 	name: string;
+	slug: string;
+	address: string;
+	city: string;
+	www: string;
+	email: string;
+	facebook: string;
+	phone: string;
+	school_description: string;
+	recrutation_description: string;
+	subject: string;
+	// ts_vector: string;
+	vovoidship: string;
 };
