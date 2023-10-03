282765f010e98f8f16ea991de6cb9f103f8b64b5
davidkupyn <davidkupyn@gmail.com>
2023-10-03 22:30:44 +0200

    yo yo yo

--- src/lib/components/universities.svelte
+++ src/lib/components/universities.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import type { University as UniversityType } from '$lib/types';
-	import { SearchX } from 'lucide-svelte';
+	import { GraduationCap, SearchX } from 'lucide-svelte';
 	import { fly, scale } from 'svelte/transition';
 	import Skeleton from './ui/skeleton.svelte';
 	import University from './university.svelte';
@@ -27,9 +27,19 @@
 			class="hidden only:flex gap-6 flex-col items-center"
 		>
 			<SearchX class="text-accent" size="36" />
-			<p class="text-center max-w-xs w-full">
+			<p class="text-center max-w-md w-full [text-wrap:balance]">
 				Unfortunately, we weren't able to find anything for you. Perhaps try searching in a
 				different way.
+			</p>
+		</div>
+	{:else if !universities.length}
+		<div
+			in:scale|global={{ start: 0.9, duration: 200 }}
+			class="hidden only:flex gap-6 flex-col items-center"
+		>
+			<GraduationCap class="text-accent" size="36" />
+			<p class="text-center max-w-md w-full [text-wrap:balance]">
+				Start by searching for a university or a major.
 			</p>
 		</div>
 	{/if}
